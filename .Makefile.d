a1a68a536dd436f74c9e10843274663e84ade41e
Philip Dexter <philip.dexter@gmail.com>
2020-06-22 11:44:13 +0300

    Add cpdttactics

---
+++ .Makefile.d
@@ -0,0 +1,9 @@
+CpdtTactics.vo CpdtTactics.glob CpdtTactics.v.beautified CpdtTactics.required_vo: CpdtTactics.v 
+CpdtTactics.vio: CpdtTactics.v 
+CpdtTactics.vos CpdtTactics.vok CpdtTactics.required_vos: CpdtTactics.v 
+Maps.vo Maps.glob Maps.v.beautified Maps.required_vo: Maps.v 
+Maps.vio: Maps.v 
+Maps.vos Maps.vok Maps.required_vos: Maps.v 
+ourlang.vo ourlang.glob ourlang.v.beautified ourlang.required_vo: ourlang.v gh/CpdtTactics.vo Maps.vo
+ourlang.vio: ourlang.v gh/CpdtTactics.vio Maps.vio
+ourlang.vos ourlang.vok ourlang.required_vos: ourlang.v gh/CpdtTactics.vos Maps.vos
